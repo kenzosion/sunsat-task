f96e2cd0118ad1687c43dae727b1c3f273f8c93f
Haur Kang <haur.kang@gmail.com>
2019-02-21 23:21:48 +0000

    creates todo item component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class TodoList extends Component {
+export default class TodoItem extends Component {
   render() {
     return (
       <div>
-        hello from todolist component
+        hello from todoitem component
       </div>
     )
   }
